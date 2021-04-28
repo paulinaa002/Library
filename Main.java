4b542d9257a47acc9bddffbf3d497a56fc836331
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-04-28 14:30:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+package library;
+
+import library.publication.Book;
+import library.publication.Magazine;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Book b1 = new Book("Shakespeare", "Hamlet", 11111, 2001, true, "novel");
+        //b1.takeBook();
+        //b1.takeBook();
+        b1.setReturnUntil(new Date());
+        System.out.println("Return until: " + b1.getReturnUntil());
+        System.out.println(b1.toString());
+
+        Magazine m1 = new Magazine("author", "title", 10101, 2011, true, 11, 2.4);
+        System.out.println(m1.toString());
+        m1.sellMagazine();
+        System.out.println(m1.toString());
+
+        Publication p;
+        p = new Publication("autorius", "pavadinimas", 12345, 2000, true);
+        /*System.out.println(p.toString());
+        p.clearData();
+        System.out.println(p.toString());
+        System.out.println();
+        p = new Book("autorius1", "pavadinimas1", 55555, 2005, true,"zanras");
+        System.out.println(p.toString());
+        p.clearData();
+        System.out.println(p.toString());
+        System.out.println();
+        p = new Magazine("autorius2", "pavadinimas2", 66666, 2006, true, 6666, 6.6);
+        System.out.println(p.toString());
+        p.clearData();
+        System.out.println(p.toString());*/
+
+       p.takePublication();
+
+
+    }
+}
