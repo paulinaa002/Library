0b775c094c4442fb180e8733078188fb9eed33be
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-05-11 15:25:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,44 +1,70 @@
 package library;
 
-import library.publication.Book;
-import library.publication.Magazine;
-
-import java.util.Date;
+import library.publication.Publication;
+import library.publication.PublicationIssueDateException;
+import library.publication.PublicationNotAvailableException;
+import library.publication.book.Book;
+import library.publication.magazine.Magazine;
 
 public class Main {
 
-    public static void main(String[] args) {
 
-        /*Book b1 = new Book("Shakespeare", "Hamlet", 11111, 2001, true, "novel");
-        //b1.takeBook();
-        //b1.takeBook();
-        b1.setReturnUntil(new Date());
-        System.out.println("Return until: " + b1.getReturnUntil());
-        System.out.println(b1.toString());
 
-        Magazine m1 = new Magazine("author", "title", 10101, 2011, true, 11, 2.4);
-        System.out.println(m1.toString());
-        m1.sellMagazine();
-        System.out.println(m1.toString());
+    public static void main(String[] args) throws CloneNotSupportedException {
 
-        Publication p;
-        p = new Publication("autorius", "pavadinimas", 12345, 2000, true);
-        /*System.out.println(p.toString());
-        p.clearData();
-        System.out.println(p.toString());
-        System.out.println();
-        p = new Book("autorius1", "pavadinimas1", 55555, 2005, true,"zanras");
-        System.out.println(p.toString());
-        p.clearData();
-        System.out.println(p.toString());
-        System.out.println();
-        p = new Magazine("autorius2", "pavadinimas2", 66666, 2006, true, 6666, 6.6);
-        System.out.println(p.toString());
-        p.clearData();
-        System.out.println(p.toString());
+        try {
+            Book book = new Book("", "Hamlet", 11122, 2001, "drama");
 
-       p.takePublication();*/
+        } catch (PublicationIssueDateException exc) {
+            exc.printStackTrace();
+            System.out.println("Wrong creation date. It is in the future");
+        } catch (LibraryException exc) {
+            exc.printStackTrace();
+        }
 
+
+        try {
+            Book book1 = new Book("Shakespeare", "Hamlet", 11122, 2025, "drama");
+
+        } catch (PublicationIssueDateException exc) {
+            exc.printStackTrace();
+            System.out.println("Wrong creation date. It is after current date (" + exc.getDate() + ")");
+        } catch (LibraryException exc) {
+            exc.printStackTrace();
+        }
+
+
+        try {
+            Book book2 = new Book("Shakespeare", "Hamlet", 11122, 2021, "drama");
+            book2.setAvailable(false);
+            book2.takePublication();
+
+        }catch (PublicationNotAvailableException exc) {
+            exc.printStackTrace();
+            System.out.println("Trying to take a book that isn't available.");
+        }catch (PublicationIssueDateException exc) {
+            exc.printStackTrace();
+            System.out.println("Wrong creation date. It is after current date (" + exc.getDate() + ")");
+        } catch (LibraryException exc){
+            exc.printStackTrace();
+        }
+
+
+        Magazine m1 = null;
+        try{
+            m1 = new Magazine("author", "title", 1122, 2001, 12);
+        } catch (LibraryException exc){
+            exc.printStackTrace();
+        }
+        if (m1 != null) {
+            Magazine m2 = (Magazine) m1.clone();
+            System.out.println(m1==m2); //ats: false, nes sukuriami 2 objektai su skirtingom nuorodom
+            System.out.println(m1.getAuthor());
+            System.out.println(m2.getAuthor());
+            System.out.println(m1.getReleaseNo());
+            System.out.println(m2.getReleaseNo());
+        }
 
     }
+
 }
